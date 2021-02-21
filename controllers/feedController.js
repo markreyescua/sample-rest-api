e10532be5fb6cea796f1361e1d313ce3f16e661c
Mark Cua <mark.r.cua@gmail.com>
2021-02-21 23:56:17 +0800

    feat: add async await

--- controllers/feedController.js
+++ controllers/feedController.js
@@ -44,29 +44,30 @@ exports.getFeeds = async (req, res, next) => {
   }
 };
 
-exports.getFeed = (req, res, next) => {
+exports.getFeed = async (req, res, next) => {
   const feedId = req.params.id;
-  Feed.findById(feedId)
-    .then((feed) => {
-      if (!feed) {
-        const error = new Error("No feed with matching id found.");
-        error.statusCode = 404;
-        throw error;
-      }
-      res.status(200).json({
-        message: "Successfully got feed!",
-        feed: feed,
-      });
-    })
-    .catch((error) => {
-      if (!error.statusCode) {
-        error.statusCode = 500;
-      }
-      next(error);
+
+  try {
+    const feed = await Feed.findById(feedId);
+    if (!feed) {
+      const error = new Error("No feed with matching id found.");
+      error.statusCode = 404;
+      throw error;
+    }
+
+    res.status(200).json({
+      message: "Successfully got feed!",
+      feed: feed,
     });
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  }
 };
 
-exports.postFeeds = (req, res, next) => {
+exports.postFeeds = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const error = new Error("Validation failed, entered data is incorrect.");
@@ -81,42 +82,35 @@ exports.postFeeds = (req, res, next) => {
   const title = req.body.title;
   const content = req.body.content;
   const imageUrl = req.file.path;
-  let creator;
   const post = new Feed({
     title: title,
     content: content,
     imageUrl: `http://localhost:3000/${imageUrl}`,
     creator: req.userId,
   });
-  post
-    .save()
-    .then(() => {
-      return User.findById(req.userId);
-    })
-    .then((user) => {
-      creator = user;
-      user.feeds.push(post);
-      return user.save();
-    })
-    .then(() => {
-      res.status(201).json({
-        message: "Successfully posted feed!",
-        feed: post,
-        creator: {
-          _id: creator._id,
-          name: creator.name,
-        },
-      });
-    })
-    .catch((error) => {
-      if (!error.statusCode) {
-        error.statusCode = 500;
-      }
-      next(error);
+
+  try {
+    await post.save();
+    const user = await User.findById(req.userId);
+    user.feeds.push(post);
+    await user.save();
+    res.status(201).json({
+      message: "Successfully posted feed!",
+      feed: post,
+      creator: {
+        _id: user._id,
+        name: user.name,
+      },
     });
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  }
 };
 
-exports.updateFeed = (req, res, next) => {
+exports.updateFeed = async (req, res, next) => {
   const errors = validationResult(req);
   if (!errors.isEmpty()) {
     const error = new Error("Validation failed, entered data is incorrect.");
@@ -127,33 +121,31 @@ exports.updateFeed = (req, res, next) => {
   const feedId = req.params.id;
   const title = req.body.title;
   const content = req.body.content;
-  Feed.findById(feedId)
-    .then((feed) => {
-      if (!feed) {
-        const error = new Error("Could not find feed.");
-        error.statusCode = 404;
-        throw error;
-      }
-      if (feed.creator.toString() !== req.userId) {
-        const error = new Error("Not authorized.");
-        error.statusCode = 403;
-        throw error;
-      }
-      feed.title = title;
-      feed.content = content;
-      return feed.save();
-    })
-    .then(() => {
-      res.status(201).json({
-        message: "Successfully updated feed!",
-      });
-    })
-    .catch((error) => {
-      if (!error.statusCode) {
-        error.statusCode = 500;
-      }
-      next(error);
+
+  try {
+    const feed = await Feed.findById(feedId);
+    if (!feed) {
+      const error = new Error("Could not find feed.");
+      error.statusCode = 404;
+      throw error;
+    }
+    if (feed.creator.toString() !== req.userId) {
+      const error = new Error("Not authorized.");
+      error.statusCode = 403;
+      throw error;
+    }
+    feed.title = title;
+    feed.content = content;
+    await feed.save();
+    res.status(201).json({
+      message: "Successfully updated feed!",
     });
+  } catch (error) {
+    if (!error.statusCode) {
+      error.statusCode = 500;
+    }
+    next(error);
+  }
 };
 
 exports.deleteFeed = (req, res, next) => {
