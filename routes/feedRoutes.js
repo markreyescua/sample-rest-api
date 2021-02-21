846f7375076865527d69d4f1cb31acbabc6f4e37
Mark Cua <mark.r.cua@gmail.com>
2021-02-21 22:42:08 +0800

    feat: add authorization

--- routes/feedRoutes.js
+++ routes/feedRoutes.js
@@ -2,13 +2,15 @@ const express = require("express");
 const { body } = require("express-validator");
 const router = express.Router();
 const controller = require("../controllers/feedController");
+const isAuth = require("../middleware/is-auth");
 
 // GET - /feeds
-router.get("/", controller.getFeeds);
+router.get("/", isAuth, controller.getFeeds);
 
 // POST - /feeds
 router.post(
   "/",
+  isAuth,
   [
     body("title").trim().isLength({ min: 5 }),
     body("content").trim().isLength({ min: 5 }),
@@ -20,6 +22,7 @@ router.get("/:id", controller.getFeed);
 
 router.put(
   "/:id",
+  isAuth,
   [
     body("title").trim().isLength({ min: 5 }),
     body("content").trim().isLength({ min: 5 }),
@@ -27,6 +30,6 @@ router.put(
   controller.updateFeed
 );
 
-router.delete("/:id", controller.deleteFeed);
+router.delete("/:id", isAuth, controller.deleteFeed);
 
 module.exports = router;
