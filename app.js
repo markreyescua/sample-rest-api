ba4b8f4b6ea561cc85d4f4430c810be6070818a9
Mark Cua <mark.r.cua@gmail.com>
2021-03-01 14:55:11 +0800

    feat: disabled helmet

--- app.js
+++ app.js
@@ -73,7 +73,7 @@ app.use((req, res, next) => {
 app.use("/feed", feedRoutes);
 app.use("/auth", authRoutes);
 
-app.use(helmet());
+// app.use(helmet());
 app.use(compression());
 
 // Error Handler
